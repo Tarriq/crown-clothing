45d45580f50041ab14e8b6e928cfd5d21acca472
Tariq El Ghayate <tarekelghayate@gmail.com>
2023-06-25 22:12:55 +0100

    publishable key

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,3 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-export const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+export const stripePromise = loadStripe(
+  'pk_test_51MDFeaFkq2VeAKuGBTy4hMebMtcTSyobCtcCLOQWhUcF8jogepgwpU2N0Fiw7751WB9idGaZoMHvkGZp8js5rf3J00huAu2US7'
+);
